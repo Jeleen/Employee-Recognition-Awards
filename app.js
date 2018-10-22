cd477eaaf9b45a6793c7adcd46917bc3dfc91dba
John Eleen <eleenj@oregonstate.edu>
2018-10-22 11:21:13 -0700

    Init Week 5 push

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+const latex = require('node-latex');
+const fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/create_award.js
+++ routes/create_award.js
@@ -4,6 +4,22 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('create_award');
+
+
+//testing
+//const input = fs.createReadStream('input.tex')
+//const output = fs.createWriteStream('output.pdf')
+//const options = {
+//	inputs: resolve(join(__dirname, './award'))}
+//latex(input, options).pipe(output);
+
+//const pdf = latex(input)
+//console.log("PDF CREATED!!");
+//pdf.pipe(output)
+//pdf.on('error', err => console.error(err))
+
+
+
 });
 
 module.exports = router;
