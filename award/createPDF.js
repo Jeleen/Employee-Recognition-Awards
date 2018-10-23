7000e6806d5ea1ab26c3f1357807d3ab3814937e
John Eleen <eleenj@oregonstate.edu>
2018-10-22 19:08:50 -0700

    Temp for paths...week 5

--- award/createPDF.js
+++ award/createPDF.js
@@ -1,21 +1,28 @@
 
+//return a nomalized path
+const path = require('path');
+const { join, resolve } =path
+const absolutePath = path.join(__dirname,'../');
 
+function createPDF(){
+	//point to node-latex
+	const latex = require('../node_modules/node-latex/');
+	//require file stream	
+	const fs = require('fs');
 
-//require node-latex
-//const latex = require('../Employee-Recognition-Awards/node_modules/node-latex/');
-const latex = require('../node_modules/node-latex/');
-const fs = require('fs');
+	//node module with working with directors
+	
 
-//node module with working with directors
- const { join, resolve } = require('path')
+	const input = fs.createReadStream(absolutePath+"award/award.tex");
+	const output = fs.createWriteStream(absolutePath+'award/output.pdf')
+	const options = {
+		inputs: resolve(join(__dirname, 'data'))}
+	latex(input, options).pipe(output);
 
-const input = fs.createReadStream("award.tex");
-const output = fs.createWriteStream('output.pdf')
-const options = {
-	inputs: resolve(join(__dirname, 'data'))}
-latex(input, options).pipe(output);
+	//const pdf = latex(input)
+	console.log("PDF CREATED!!");
+	//pdf.pipe(output)
+	//pdf.on('error', err => console.error(err))
+};
 
-//const pdf = latex(input)
-//console.log("PDF CREATED!!");
-//pdf.pipe(output)
-//pdf.on('error', err => console.error(err))
+module.exports = createPDF;
--- routes/create_award.js
+++ routes/create_award.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var awardCreate = require('../award/createPDF.js');
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
- // res.render('create_award')
-
+	console.log("Inside the get route for award");
+	awardCreate();  
+	res.render('create_award')
 
 });
 
