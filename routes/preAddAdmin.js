be5e3ef8ca69a1be5f24da8c20ca80a17ef8e360
deirdreemoran <deirdreemoran@gmail.com>
2018-10-29 01:24:59 -0700

    Removed unnecesary routes and hbs

--- routes/preAddAdmin.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/preAddAdmin', function(req, res, next) {
-	res.render('preAddAdmin', { title: "Accounts" , layout: "preAddAdminLayout.hbs" });
-});
-module.exports = router;
--- routes/preAddUser.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/preAddUser', function(req, res, next) {
-	res.render('preAddUser', { title: "Accounts" , layout: "preAddUserLayout.hbs" });
-});
-module.exports = router;
