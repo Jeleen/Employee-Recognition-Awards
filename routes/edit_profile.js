cde64dd4b5b9250a17817542b293bb5bc5904e1f
elchapulincolorado <tynerl@oregonstate.edu>
2018-11-17 21:19:12 -0600

    Week 9 forgot password page and email functionality, Navbar on User pages, move existing awards to User dashboard

--- routes/edit_profile.js
+++ routes/edit_profile.js
@@ -3,25 +3,22 @@ var router = express.Router();
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({ uploadDir: './images' });
 var fs = require('fs');
+const moment =require('../node_modules/moment')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   if (req.session.isAdmin) {
     res.redirect('/login');
   }
-  appRepo.getAllAwardsCreatedBy(req.session.loggedInId)
-    .then((usersAwards) => {
-      console.log(usersAwards);
-      appRepo.getUserById(req.session.loggedInId)
-        .then(user => res.render('edit_profile', { existing: user.email, awards: usersAwards }))
-        .catch(() => res.render('edit_profile', { existing: '(error no user found)' }));
-    });
+  appRepo.getUserById(req.session.loggedInId)
+    .then(user => res.render('edit_profile', { existing: user.email }))
+    .catch(() => res.render('edit_profile', { existing: '(error no user found)' }));
 });
 
 router.post('/', function(req, res, next) {
   var newEmail = req.body.new_email;
   appRepo.editUser(req.session.loggedInId, newEmail)
-    .then(() => res.render('edit_profile', { existing: newEmail }))
+    .then(() => res.render('edit_profile', { existing: newEmail, info: "Email successfully updated" }))
     .catch((error) => console.log("Error editing user email: " + error));
 });
 
@@ -36,14 +33,6 @@ router.post('/upload', multipartMiddleware, function(req, res) {
       });
     })
     .catch(error => console.log("Error uploading user image: ", error));
-});
-
-router.post('/delete', function(req, res) {
-  appRepo.removeAwardsOfUser(req.session.loggedInId)
-  .then(() => {
-    res.render('edit_profile', { info: "Awards Successfully Deleted"})
-  })
-  .catch(error => console.log("Error deleting user's awards: ", error));
 });
 
 module.exports = router;
