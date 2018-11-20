737f45f181fefda6666ec3214ffd392be08b66d3
John Eleen <eleenj@oregonstate.edu>
2018-11-19 21:56:11 -0800

    modified routes to work

--- award/awardObject.js
+++ award/awardObject.js
@@ -43,6 +43,7 @@ function getCreator(obj){
       obj.creator_name = cName.name
       console.log("Before", cName.sig_image_path);
       var sig_path= cName.sig_image_path.substring(cName.sig_image_path.lastIndexOf("sigs"));
+    //  console.log(sig_path);
     //  cName.sig_image_path.substring(0, cName.sig_image_path.lastIndexOf("/"));
       obj.creator_sig=sig_path
       array.push(obj)
