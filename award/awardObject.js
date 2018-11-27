cb083316831ea7fad8b5c43fb452fc61938092b7
John Eleen <eleenj@oregonstate.edu>
2018-11-26 21:40:16 -0800

    default image used

--- award/awardObject.js
+++ award/awardObject.js
@@ -42,8 +42,13 @@ function getCreator(obj){
       console.log("WHAT IS IN cNAME", cName);
       obj.creator_name = cName.name
       console.log("Before", cName.sig_image_path);
-      var sig_path= cName.sig_image_path.substring(cName.sig_image_path.lastIndexOf("image"));
-    //  console.log(sig_path);
+      if(cName.sig_image_path == null){
+		console.log("FUCK YOU");
+		var sig_path = 'image/tempImage.png'
+     }else{     
+      var sig_path= cName.sig_image_path.substring(cName.sig_image_path.lastIndexOf("image"))
+	};
+      console.log(sig_path);
     //  cName.sig_image_path.substring(0, cName.sig_image_path.lastIndexOf("/"));
       obj.creator_sig=sig_path
       array.push(obj)
